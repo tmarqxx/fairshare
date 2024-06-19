28914dcdb722bd4ab75b22d0269c27e87239ca80
tmarqxx <tgmarquise@gmail.com>
2024-06-18 21:35:09 -0400

    fix: now using createRoot instead of ReactDOM.render

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -14,7 +14,9 @@ if (process.env.NODE_ENV !== "test") {
 
 const queryClient = new QueryClient();
 
-ReactDOM.render(
+const root = createRoot(document.getElementById("root")!);
+
+root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <ChakraProvider>
@@ -23,8 +25,7 @@ ReactDOM.render(
         </Router>
       </ChakraProvider>
     </QueryClientProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
